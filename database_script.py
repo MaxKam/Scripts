8f55330c30bbf3532361fd35023406dff2c6fda3
Masya120 <maxk893@gmail.com>
2017-08-24 21:38:42 -0700

    Modified script to connect to PostgreSQL

--- database_script.py
+++ database_script.py
@@ -1,22 +1,24 @@
-# Script for creating and updating a SQLite DB.
-import sqlite3
+# Script for creating and updating a PostgreSQL DB
+import psycopg2
+
+connection_string = "dbname='testdb' user='test' password='test' host='localhost' port='5432'"
 
 def create_table():
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(connection_string)
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item,quantity,price):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(connection_string)
     cur = conn.cursor()
     cur.execute("INSERT INTO store VALUES (?,?,?)",(item,quantity,price))
     conn.commit()
     conn.close()
 
 def view():
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(connection_string)
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
     rows = cur.fetchall()
@@ -24,14 +26,14 @@ def view():
     return rows
 
 def delete(item):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(connection_string)
     cur = conn.cursor()
     cur.execute("DELETE FROM store WHERE item=?",(item,))
     conn.commit()
     conn.close()
 
 def update(quantity, item):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(connection_string)
     cur = conn.cursor()
     cur.execute("UPDATE store SET quantity=? WHERE item=?", (quantity, item))
     conn.commit()
